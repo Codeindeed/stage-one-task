983ab9a734c578c17e9d4386e1e8bf5ab1b106f4
Codeindeed <favouralex084@gmail.com>
2023-09-09 18:32:22 +0100

    Edited the set interval function

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 let date = document.querySelector(".day").getElementsByTagName("p");
 let time = document.querySelector(".time").getElementsByTagName("p");
-let day = new Date();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let today = days[day.getDay()];
-date[0].textContent = today;
 
-let hours = day.getHours();
-let minutes = day.getMinutes();
-let seconds = day.getSeconds();
+function updateDay() {
+  const day = new Date();
+  let today = day.toLocaleDateString("en", { weekday: "long" });
+  date[0].textContent = today;
+  let hours = day.getHours();
+  let minutes = day.getMinutes();
+  let seconds = day.getSeconds();
+  console.log(seconds);
+  const meridian = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  time[0].textContent = hours + ":" + minutes + ":" + seconds + meridian;
+}
 
-let meridian = hours >= 12 ? "PM" : "AM";
-hours = hours % 12;
-hours = hours ? hours : 12;
-minutes = minutes < 10 ? "0" + minutes : minutes;
-time[0].textContent = hours + ":" + minutes + " " + meridian;
+setInterval(updateDay, 1000);
