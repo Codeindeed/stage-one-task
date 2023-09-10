1fe20da6e5025b23fef7644c19c1408ee113494b
Codeindeed <favouralex084@gmail.com>
2023-09-10 07:35:14 +0100

    utc time updated

--- index.js
+++ index.js
@@ -4,16 +4,10 @@ let time = document.querySelector(".time").getElementsByTagName("p");
 function updateDay() {
   const day = new Date();
   let today = day.toLocaleDateString("en", { weekday: "long" });
+  let days = Date.now();
   date[0].textContent = today;
-  let hours = day.getHours();
-  let minutes = day.getMinutes();
-  let seconds = day.getSeconds();
-  console.log(seconds);
-  const meridian = hours >= 12 ? "PM" : "AM";
-  hours = hours % 12;
-  hours = hours ? hours : 12;
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-  time[0].textContent = hours + ":" + minutes + ":" + seconds + meridian;
+  time[0].textContent = days;
+  console.log(days);
 }
 
 setInterval(updateDay, 1000);
